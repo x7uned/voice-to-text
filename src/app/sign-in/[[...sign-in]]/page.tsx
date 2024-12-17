1580d718001753187190015d03d1574cf1f4a376
x7uned <denis.goptsii@gmail.com>
2024-12-17 17:48:46 +0200

    little fixes for debug

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-	return <SignIn />
+	return (
+		<div className='flex w-screen h-screen items-center justify-center'>
+			<SignIn />
+		</div>
+	)
 }
