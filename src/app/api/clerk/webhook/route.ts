112569f31012ccc65ef6fe39be4f1e74d497a512
x7uned <denis.goptsii@gmail.com>
2024-12-18 12:56:39 +0200

    route reforming

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,49 +1,72 @@
-import { auth, currentUser } from '@clerk/nextjs/server'
-import { PrismaClient } from '@prisma/client'
-import { NextResponse } from 'next/server'
+import { createUser } from '@/lib/users'
+import { WebhookEvent } from '@clerk/nextjs/server'
+import { User } from '@prisma/client'
+import { headers } from 'next/headers'
+import { Webhook } from 'svix'
 
-// const CLERK_WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET!
+export async function POST(req: Request) {
+	const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET
 
-const prisma = new PrismaClient()
+	if (!WEBHOOK_SECRET) {
+		throw new Error(
+			'Please add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
+		)
+	}
 
-export async function POST() {
-	try {
-		const { userId } = await auth()
+	// Get the headers
+	const headerPayload = headers()
+	const svix_id = headerPayload.get('svix-id')
+	const svix_timestamp = headerPayload.get('svix-timestamp')
+	const svix_signature = headerPayload.get('svix-signature')
 
-		if (!userId) {
-			return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
-		}
-
-		const user = await currentUser()
-
-		if (!user) {
-			return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
-		}
-
-		let dbUser = await prisma.user.findUnique({
-			where: {
-				clerkId: user.id,
-			},
+	// If there are no headers, error out
+	if (!svix_id || !svix_timestamp || !svix_signature) {
+		return new Response('Error occurred -- no svix headers', {
+			status: 400,
 		})
+	}
 
-		if (!dbUser) {
-			dbUser = await prisma.user.create({
-				data: {
-					clerkId: user.id,
-					email: user.emailAddresses[0].emailAddress ?? '',
-				},
+	// Get the body
+	const payload = await req.json()
+	const body = JSON.stringify(payload)
+
+	// Create a new Svix instance with your secret.
+	const wh = new Webhook(WEBHOOK_SECRET)
+
+	let evt: WebhookEvent
+
+	// Verify the payload with the headers
+	try {
+		evt = wh.verify(body, {
+			'svix-id': svix_id,
+			'svix-timestamp': svix_timestamp,
+			'svix-signature': svix_signature,
+		}) as WebhookEvent
+	} catch (err) {
+		console.error('Error verifying webhook:', err)
+		return new Response('Error occurred', {
+			status: 400,
+		})
+	}
+
+	const eventType = evt.type
+
+	if (eventType === 'user.created') {
+		const { id, email_addresses } = evt.data
+
+		if (!id || !email_addresses) {
+			return new Response('Error occurred -- missing data', {
+				status: 400,
 			})
 		}
 
-		return new NextResponse(null, {
-			status: 302,
-			headers: { Location: 'https://voice-to-text-five.vercel.app/' },
-		})
-	} catch (error) {
-		console.error('Error saving user:', error)
-		return NextResponse.json(
-			{ message: 'Internal Server Error' },
-			{ status: 500 }
-		)
+		const user = {
+			clerkId: id,
+			email: email_addresses[0].email_address,
+		}
+
+		await createUser(user as User)
 	}
+
+	return new Response('', { status: 200 })
 }
